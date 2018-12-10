78643b2b6fc9edc7ed9f4e254316675e16ecdd61
Michael Hsu (徐承志) <evenchange4@gmail.com>
2018-12-10 10:11:51 +0800

    update

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,12 @@
 // @flow
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+
+/**
+ * ref: https://github.com/storybooks/storybook/tree/master/addons/storyshots/storyshots-core#configure-jest-to-work-with-webpacks-requirecontext
+ */
+
+registerRequireContextHook();
 
 Enzyme.configure({ adapter: new Adapter() });
